d0b880031410ad296648ae853827d05d86fef720
Jen V <89663398+Jen-V@users.noreply.github.com>
2022-02-12 21:09:41 -0500

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,24 @@
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton?) {
+        print(sender?.currentAttributedTitle)
+        playSound()
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+}
+
